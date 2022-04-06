84fec22af8c2ecbb6dff0478134e581529a9f1e2
Anna Trofimenkova <annatrofimenkova@yandex.ru>
2022-04-06 19:15:59 +0300

    апрель 2

--- src/components/api.js
+++ src/components/api.js
@@ -1,43 +1,43 @@
 
-const configCard = {
-  baseUrl: 'https://nomoreparties.co/v1/plus-cohort-8/cards',
-  headers: {
-    authorization: '837c0be1-5609-4c04-b384-491cd26df7eb'
-  }
-}
+// const configCard = {
+//   baseUrl: 'https://nomoreparties.co/v1/plus-cohort-8/cards',
+//   headers: {
+//     authorization: '837c0be1-5609-4c04-b384-491cd26df7eb'
+//   }
+// }
 
-const configName = {
-  baseUrl: 'https://nomoreparties.co/v1/plus-cohort-8/users/me ',
-  headers: {
-    authorization: '837c0be1-5609-4c04-b384-491cd26df7eb'
-  }
-}
+// const configName = {
+//   baseUrl: 'https://nomoreparties.co/v1/plus-cohort-8/users/me ',
+//   headers: {
+//     authorization: '837c0be1-5609-4c04-b384-491cd26df7eb'
+//   }
+// }
 
-const onResponce = (res) => {
-  return res.ok ? res.json() : Promise.reject(res)
-}
-
-
-export function getCard() {
-  return fetch(configCard.baseUrl, {
-    headers: configCard.headers
-  })
-    .then(onResponce)
-}
-
-export function getName() {
-  return fetch(configName.baseUrl, {
-    headers: configName.headers
-  })
-    .then(onResponce)
-}
+// const onResponce = (res) => {
+//   return res.ok ? res.json() : Promise.reject(res)
+// }
 
 
-export function editName(dataId) {
-  return fetch(`${configName.baseUrl}/${dataId}`, {
-    method: 'PATCH',
-    headers: configName.headers,
-    body: JSON.stringify(data)
-  })
-    .then(onResponce)
-}
+// export function getCard() {
+//   return fetch(configCard.baseUrl, {
+//     headers: configCard.headers
+//   })
+//     .then(onResponce)
+// }
+
+// export function getName() {
+//   return fetch(configName.baseUrl, {
+//     headers: configName.headers
+//   })
+//     .then(onResponce)
+// }
+
+
+// export function editName(dataId) {
+//   return fetch(`${configName.baseUrl}/${dataId}`, {
+//     method: 'PATCH',
+//     headers: configName.headers,
+//     body: JSON.stringify(data)
+//   })
+//     .then(onResponce)
+// }
